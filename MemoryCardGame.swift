a9188d118e307ff15aa6b5158e0a935faac4fe2a
Sirarpi Bayramyan <Sirarpi1997@gmail.com>
2019-09-11 15:41:44 +0400

    Initial commit

---
+++ MemoryCardGame.swift
@@ -0,0 +1,61 @@
+//
+//  MemoryCardGame.swift
+//  memoryCardGame
+//
+//  Created by Sirarpi on 9/11/19.
+//  Copyright © 2019 Sirarpi. All rights reserved.
+//
+
+import Foundation
+
+class MemoryCardGame {
+    
+    var cards = [Card]()
+    
+    init(numberOfPairsOfcards: Int){
+        for _ in 1...numberOfPairsOfcards {
+            let card = Card()
+            cards += [card, card]
+        }
+        shuffleCards()
+    }
+    
+    var indexOfOneAndOnluFaceUpCard: Int?
+    
+    func cooseCard(at index: Int) {
+        
+        if !cards[index].isMatched {
+    
+            if let matchedIndex = indexOfOneAndOnluFaceUpCard, matchedIndex != index {
+                
+                if cards[matchedIndex].identifier == cards[index].identifier {
+
+                    cards[matchedIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+
+                cards[index].isFaceUp = true
+                indexOfOneAndOnluFaceUpCard = nil
+               
+                
+            } else {
+                
+                    for flipDownIndex in cards.indices {
+                        cards[flipDownIndex].isFaceUp = false
+                    }
+                    cards[index].isFaceUp = true
+                    indexOfOneAndOnluFaceUpCard = index
+                 }
+            
+           }
+        
+    }
+    
+    //Shuffle the cards
+    func shuffleCards (){
+        for _ in 1...cards.count {
+            let randomIndex = Int(arc4random_uniform(UInt32(cards.count)))
+            cards.swapAt(0, randomIndex)
+        }
+    }
+}
---
+++ memoryCardGame/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  memoryCardGame
+//
+//  Created by Sirarpi on 9/11/19.
+//  Copyright © 2019 Sirarpi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    lazy  var game = MemoryCardGame(numberOfPairsOfcards: (cardButtons.count + 1) / 2)
+    
+    var flipCount = 0 {
+        didSet{
+            flipCountLabel.text = "Flip: \(flipCount)"
+        }
+    }
+    
+    var emojiCoices = ["🍌","🥥","🍊","🍓","🍇","🍒","🍐","🥝","🍏","🍍"]
+    
+    var emoji = [Int:String]()
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    
+    
+    @IBAction func touchButton(_ sender: UIButton) {
+        flipCount += 1 //"🌼"
+      if  let cardNumber = cardButtons.firstIndex(of: sender)
+      {
+        game.cooseCard(at: cardNumber)
+        updateViewFromModel()
+      } else{
+        print("chosen card was not in cardButtons")
+        }
+    }
+    func updateViewFromModel(){
+        for index in cardButtons.indices {
+            
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emojiChoice(for: card), for: UIControl.State.normal)
+                button.backgroundColor = #colorLiteral(red: 0.9569379687, green: 0.8365390897, blue: 0.979655683, alpha: 1)
+                
+            }else {
+                
+                button.setTitle("", for: UIControl.State.normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 0.1298420429, green: 0.1298461258, blue: 0.1298439503, alpha: 0): #colorLiteral(red: 0.3236978054, green: 0.1063579395, blue: 0.574860394, alpha: 1)
+            }
+            
+        }
+    }
+    
+    
+    func emojiChoice(for card : Card) -> String {
+        if emoji[card.identifier] == nil, emojiCoices.count > 0 {
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiCoices.count)))
+            emoji[card.identifier] = emojiCoices.remove(at: randomIndex)
+            
+        }
+        return emoji[card.identifier] ?? "?"
+    }
+    
+    @IBAction func touchNewGame(_ sender: UIButton) {
+        flipCount = 0
+        game = MemoryCardGame(numberOfPairsOfcards: (cardButtons.count + 1 ) / 2) //new game
+        
+        updateViewFromModel() //update from Model
+    }
+}
+
